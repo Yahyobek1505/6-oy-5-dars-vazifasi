4ba23d25e39bb69468520cbe82f827b18bd664db
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-02-17 20:27:31 +0500

    Homework is start!

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <header>
+          <div className="header">
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+          <div className="naigation">
+          <ul>
+            <li>Vakansiyalar</li>
+            <li>Kandidatlar</li>
+            <li>Kompaniyalar</li>
+            <li>Vakansiyalar</li>
+            <li>Ta'lim</li>
+          </ul>
+          </div>
+          <div className="login">
+            <span>O'zb</span>
+            <button className="btn">Boshlash</button>
+          </div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
