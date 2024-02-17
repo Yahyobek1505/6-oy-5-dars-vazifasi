f20ab120d84c55a61be24eeededb9bbab73712e2
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-02-18 01:25:47 +0500

    Homework is almost done!

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,33 @@ function App() {
   return (
     <>
       <div className="container">
-        <header>
-          <div className="header">
+        <div className="header">
           <div className="logo">
-            <img src="" alt="" />
+            <img src="https://picsum.photos/200/300?image=102" alt="" />
           </div>
           <div className="naigation">
-          <ul>
-            <li>Vakansiyalar</li>
-            <li>Kandidatlar</li>
-            <li>Kompaniyalar</li>
-            <li>Vakansiyalar</li>
-            <li>Ta'lim</li>
-          </ul>
+            <ul className="nav-menu">
+              <li>Vakansiyalar</li>
+              <li>Kandidatlar</li>
+              <li>Kompaniyalar</li>
+              <li>Vakansiyalar</li>
+              <li>Ta'lim</li>
+            </ul>
           </div>
           <div className="login">
             <span>O'zb</span>
             <button className="btn">Boshlash</button>
           </div>
-          </div>
-        </header>
+        </div>
       </div>
+        <div className="main-section">
+          <h2 className="title">Kompaniya ma'lumotlari</h2>
+          <p className="comment">Kompaniya haqidagi ma'lumotlarni kiriting</p>
+          <span className="acount">
+            <img className="circle" src="https://picsum.photos/200/300" alt="" />
+            <a href="#">Yuklash</a>
+          </span>
+        </div>
     </>
   );
 }
